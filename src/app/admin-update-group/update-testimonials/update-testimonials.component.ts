1e6f026cc646028805b4bdb0aca80924389b5d9f
Javed Ansari <ansari.nitkkr786@gmail.com>
2024-08-11 17:17:29 +0530

    Added update/delete page for admin

---
+++ src/app/admin-update-group/update-testimonials/update-testimonials.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-testimonials',
+  templateUrl: './update-testimonials.component.html',
+  styleUrls: ['./update-testimonials.component.css']
+})
+export class UpdateTestimonialsComponent {
+
+}
